a47c0bbd582c3453db2a24f52eae95b965f52c52
Shoaib khan <shoibkhan7900@gmail.com>
2026-01-14 00:25:24 +0530

    Remove duplicate python files form root directory

--- hello.py
+++
@@ -1,3 +0,0 @@
-# Hello World program in python
-
-print("Hello World")
--- sum.py
+++
@@ -1,6 +0,0 @@
-# Program to calculate sum of two numbers
-
-a=int(input("Enter a first number:"))
-b=int(input("Enter second number:"))
-c=a+b
-print("sum is:",c)
